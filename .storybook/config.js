c1041030e68f77c1256c81d73fc09eec4811631c
Phil Cockfield <phil@cockfield.net>
2017-05-21 12:15:38 +1200

    Loading with explicit story imports.

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,20 @@ if (typeof window === 'object') window.__storybook_stories__ = require('@kadira/
 
 
 // Load stories.
-const req = require.context("../lib", true, /.stories.js$/);
-configure(() => {
-  req.keys().forEach(filename => req(filename));
-}, module);
+// See:
+//    https://github.com/storybooks/storybook/issues/125#issuecomment-212386581
+
+// const req = require.context("../lib", true, /.stories.js$/);
+// configure(() => {
+//   req.keys().forEach(filename => req(filename));
+// }, module);
+
+
+function loadStories() {
+  require('../lib/index.stories.js');
+  // You can require as many stories as you need.
+}
+
+configure(loadStories, module);
+
+
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Sample of Percy using React
 
 https://gist.github.com/timhaines/f192a0b4f8be4c7ec6396708ae25f36d
+
+
+
+Config: `xxx.stories.js`
+  https://github.com/storybooks/storybook/issues/125#issuecomment-212386581
