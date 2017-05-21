b839385f2d2d893631bec9236486b546ea76958e
Phil Cockfield <phil@cockfield.net>
2017-05-21 14:43:58 +1200

    Update refs

--- src/components/Bar/Bar.stories.tsx
+++ src/components/Bar/Bar.stories.tsx
@@ -1,12 +1,23 @@
-import { React, describe } from '../../common/storybook';
-import { Bar } from '.';
+// import { React, describe } from '../../common/storybook';
+// import { Bar } from '.';
 
 
-describe('primtives', {
-  title: 'A sample Bar component.',
-})
-  .add('Bar', () => {
-    return (
-      <Bar />
-    );
-  });
+// describe('primtives', {
+//   title: 'A sample Bar component.',
+// })
+//   .add('Bar', () => {
+//     return (
+//       <Bar />
+//     );
+//   });
+
+import * as React from 'react';
+import { storiesOf, action } from '@kadira/storybook';
+
+storiesOf('Bar', module)
+  .add('with text', () => (
+    <button onClick={action('clicked')}>Hello Button</button>
+  ))
+  .add('with some emoji', () => (
+    <button onClick={action('clicked')}>😀 😎 👍 💯</button>
+  ));
